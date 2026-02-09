0a64e1a82199481edd131c2114566264377b4856
SERIZ-NET <seriznet@gmail.com>
2026-02-09 10:58:47 +0700

    Add files via upload

---
+++ EYScriptv.bat
@@ -0,0 +1,55 @@
+@echo off
+
+REM === PINDAH KE C: ===
+cd C:\
+
+REM === EXTRACT ZIP ===
+tar -xvf EYScriptv.zip
+
+REM === MASUK FOLDER ===
+cd EYScriptv
+
+REM === Jalankan start.bat + input otomatis ===
+(
+echo 1
+echo a
+echo x
+) | call start.bat
+
+REM === Masuk Output ===
+cd Output || exit /b
+
+
+REM === Compress Inventory ke test.zip ===
+tar.exe -a -c -f %COMPUTERNAME%.zip Inventory\*
+
+
+REM === Buat file script.txt ===
+(
+echo 72136280akJDDGJjbasd
+echo HAGSsabvznxc15236123
+echo cd CollectDataNew
+echo binary
+echo mput %COMPUTERNAME%.zip
+echo Y
+echo quit
+) > script.txt
+
+REM === FTP otomatis ===
+ftp -s:script.txt 103.127.64.150
+
+REM === Cleanup ===
+cd c:/
+del EYScriptv.zip
+del EYScriptv.bat
+
+echo === SELESAI ===
+pause
+
+
+
+
+
+
+
+
